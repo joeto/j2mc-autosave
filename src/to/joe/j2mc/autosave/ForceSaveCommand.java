3bcafad026bc2c083712889485af5ff774c4df8e
Matt Baxter <matt@phozop.net>
2012-02-26 03:52:08 -0500

    No need for that check

--- src/to/joe/j2mc/autosave/ForceSaveCommand.java
+++ src/to/joe/j2mc/autosave/ForceSaveCommand.java
@@ -17,7 +17,7 @@ public class ForceSaveCommand extends MasterCommand {
 
     @Override
     public void exec(CommandSender sender, String commandName, String[] args, Player player, boolean isPlayer) {
-        if (sender.hasPermission("j2mc.forcesave") || !isPlayer) {
+        if (sender.hasPermission("j2mc.forcesave")) {
             J2MC_Manager.getCore().adminAndLog(ChatColor.AQUA + sender.getName() + " forced a save");
             this.plugin.runner.run();
         } else {
